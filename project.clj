9f127db447c4800c2f610b3c9c1a0dbdc220e647
lread <lee@dlread.com>
2022-09-23 19:32:42 -0400

    Release: updates for version 1.1.29

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.lread/muckabout "1.1.28"
+(defproject com.github.lread/muckabout "1.1.29"
   :description "Muckabout"
   :url "https://github.com/lread/muckabout"
   :license {:name "Eclipse Public License - v 1.0"
