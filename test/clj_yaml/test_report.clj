4f3870ef6ba86f910f55855175700f7f5ac7317f
lread <lee@dlread.com>
2023-03-10 14:38:13 -0500

    New test: github actions push/pull_request trigger
    Baseline commit.
    Things are not working how we'd like them to.
    
    Goal: Always run tests once for pushed commit(s).
    This should be the case for:
    - push to master on this repo
    - push to branch on this repo
    - push to fork of this repo
    - push to PR of this repo
    
    Currently GHA is building twice on push to PR.
    Once for PR and once for push.
    
    I'd like to avoid this waste of compute.
    Also, it is confusing.

--- test/clj_yaml/test_report.clj
+++
@@ -1,9 +0,0 @@
-(ns clj-yaml.test-report
-  (:require [clojure.test]))
-
-(def platform
-  (str "jvm-clj " (clojure-version)))
-
-(defmethod clojure.test/report :begin-test-var [m]
-  (let [test-name (-> m :var meta :name)]
-    (println (format "=== %s [%s]" test-name platform))))
